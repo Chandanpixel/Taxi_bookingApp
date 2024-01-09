e5c3cb364441aa7d77fdf33d1b95fc7ac4803179
chandan <sahuchandan587@gmail.com>
2024-01-09 14:57:53 +0530

    code updated

---
+++ src/com/qapractice/Car.java
@@ -0,0 +1,21 @@
+package com.qapractice;
+
+import java.util.Collections;
+
+public class Car {
+
+    public static void main(String[] args) {
+
+        String name = "Audi";
+        int price = 5000000;
+
+        System.out.println(name);
+        System.out.println(price);
+
+
+
+
+
+
+    }
+}
